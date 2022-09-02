3cd7e496475ece9baccb1097863e0d3c405ee3fc
Itamar Sharify <itamarsharify@gmail.com>
2022-09-03 02:12:49 +0300

    fix: typing errors

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -6,9 +6,14 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { page } = req.query;
-  const start = (parseInt(page) - 1) * 10;
+  const pageNumber = page ? parseInt(page as string) : 1;
+  const start = (pageNumber - 1) * 10;
   const end = start + 10;
   const parser: Parser = new Parser();
+  if (!process.env.RSS) {
+    res.status(500).json({ error: "Missing RSS URL" });
+    return;
+  }
   const rss = await parser.parseURL(process.env.RSS);
   const { items } = rss;
   const episodes = items.slice(start, end);
