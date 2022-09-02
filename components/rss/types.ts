8eaef65696e4d109798fcf1bbe8ee229c6f7a56b
Itamar Sharify <itamarsharify@gmail.com>
2022-09-03 02:07:44 +0300

    fix: typing errors

--- components/rss/types.ts
+++ components/rss/types.ts
@@ -1,13 +1,13 @@
 export type EpisodeData = {
-  guid: string | null;
-  title: string | null;
-  link: string | null;
-  content: string | null;
-  pubDate: string | null;
-  duration: string | null;
-  imageUrl: string | null;
-  audioUrl: string | null;
-  isoDate: string | null;
+  guid: string;
+  title: string;
+  link: string;
+  content: string;
+  pubDate: string;
+  duration: string;
+  imageUrl: string;
+  audioUrl: string;
+  isoDate: string;
   itunes: { image: string; duration: string };
   enclosure: { url: string };
 };
