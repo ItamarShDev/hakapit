81803df46f5f93c633957d6144b3d34afa0aa3ff
Itamar Sharify <itamarsharify@gmail.com>
2022-09-03 22:42:37 +0300

    fix: og image

--- pages/episodes/[slug].tsx
+++ pages/episodes/[slug].tsx
@@ -1,10 +1,21 @@
 import { Episode } from "components/rss/Episode";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Head from "next/head";
 import { fetchEpisode } from "pages/api/feed";
 export default function Index({
   rss,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return <Episode episode={rss.episode} />;
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content={`https://hakapit.tech/api/og-image?title=${rss.episode.title}`}
+        />
+      </Head>
+      <Episode episode={rss.episode} />;
+    </>
+  );
 }
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   if (!process.env.RSS) return { props: {} };
