826b98c55230a3c23c3669588e511e4c068db012
Itamar Sharify <itamarsharify@gmail.com>
2022-09-03 22:17:54 +0300

    fix: og image

--- pages/api/og-image.tsx
+++ pages/api/og-image.tsx
@@ -2,22 +2,28 @@ import { withOGImage } from "next-api-og-image";
 
 type QueryParams = {
   title: string;
-  logo: string;
+  // logo: string;
 };
 
 export default withOGImage<"query", QueryParams>({
   strategy: "query",
   template: {
-    react: ({ title }) => {
-      return (
-        <html>
-          <body
-            style={{ direction: "rtl", backgroundColor: "red", color: "white" }}
-          >
-            <span>{title}</span>
-          </body>
-        </html>
-      );
-    },
+    html: ({ title }) => `<span>${title}
+                          <style>
+                          direction: rtl;
+                          background-color: red;
+                          color: white;
+                          </style></span>`,
+    // {
+    //   return (
+    //     <html>
+    //       <body
+    //         style={{ direction: "rtl", backgroundColor: "red", color: "white" }}
+    //       >
+    //         <span>{title}</span>
+    //       </body>
+    //     </html>
+    //   );
+    // },
   },
 });
