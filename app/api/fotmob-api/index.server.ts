d34c3604e6b29a1b762c6dc3da3eff110ff0d6df
Itamar Sharify <itamar.sharify@imubit.com>
2024-01-24 00:00:37 +0200

    chg: remove local fotmob code

--- app/api/fotmob-api/index.server.ts
+++ app/api/fotmob-api/index.server.ts
@@ -1,15 +1,13 @@
 import Fotmob from "fotmob";
 import { LiverpoolId } from "~/api/fotmob-api/constants";
-import FotmobAPI from "~/api/fotmob-api/src/fotmob";
-import type { Team } from "~/api/fotmob-api/src/types/team";
 
 export async function getTeam() {
 	const fotmob = new Fotmob();
-	return (await fotmob.getTeam(8650)) as Team;
+	return await fotmob.getTeam(8650);
 }
 
 export function getLeague(league: number) {
-	const fotmob = new FotmobAPI();
+	const fotmob = new Fotmob();
 	return fotmob.getTeamSeasonStats(LiverpoolId, league);
 }
 
--- app/api/fotmob-api/src/type-cast-error.ts
+++
@@ -1,6 +0,0 @@
-export class CastingError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = "CastingError";
-	}
-}
