e138e23aad999db96c58fe96a12cd575d68aa380
Itamar Sharify <itamar.sharify@imubit.com>
2024-01-24 20:46:27 +0200

    fix: remove redundant header state

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Link } from "@remix-run/react";
-import { useEffect, useState } from "react";
 import MenuIcon from "~/components/icons/menu";
 import { Links } from "~/components/links";
 import { Podcast } from "~/db/types";
@@ -11,12 +10,6 @@ type Props = {
 } & React.HTMLAttributes<HTMLElement>;
 export default function Header({ data, podcast, className }: Props) {
 	const { imageUrl } = data;
-	const [linksShown, setLinksShown] = useState(false);
-
-	useEffect(() => {
-		if (podcast) setLinksShown(false);
-	}, [podcast]);
-
 	return (
 		<header className="overflow-hidden header">
 			<div className="flex flex-wrap items-start gap-4 p-4 lg:items-center ">
@@ -29,13 +22,13 @@ export default function Header({ data, podcast, className }: Props) {
 					</Link>
 				</div>
 				<div className="flex flex-col flex-wrap items-end gap-2 pt-2 lg:gap-4 lg:hidden">
-					<Button variant="link" onClick={() => setLinksShown((prev) => !prev)}>
+					<Button variant="link" className="menu-button">
 						<MenuIcon />
 					</Button>
 				</div>
 				<Links className="flex-row hidden lg:flex" />
 			</div>
-			<div className={cn("grid items-start grid-transition", linksShown ? "show-menu" : "hide-menu")}>
+			<div className={cn("grid items-start grid-transition header-links hide-menu")}>
 				<Links className="flex flex-col items-center text-2xl" />
 			</div>
 		</header>
