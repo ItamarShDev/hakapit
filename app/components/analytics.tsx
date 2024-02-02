387265413d64e787dfb2840df5e5d1fe0dc253c3
Itamar Sharify <itamar.sharify@imubit.com>
2024-02-02 09:46:44 +0200

    Update SpeedInsights import in analytics.tsx

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/remix";
 
 export function AnalyticsWrapper() {
 	return (
