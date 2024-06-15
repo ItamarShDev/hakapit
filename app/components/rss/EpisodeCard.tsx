e0ce1219f17ef5216a670915f324384400b3d315
Itamar Sharify <itamarsharify@gmail.com>
2024-06-15 14:10:51 +0300

    chg: episode card bg color

--- app/components/rss/EpisodeCard.tsx
+++ app/components/rss/EpisodeCard.tsx
@@ -27,6 +27,19 @@ export function SkeletonCard({ className }: React.HTMLAttributes<HTMLDivElement>
 	);
 }
 
+const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+const triplet = (e1: number, e2: number, e3: number) =>
+	keyStr.charAt(e1 >> 2) +
+	keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+	keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+	keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r: number, g: number, b: number) =>
+	`data:image/gif;base64,R0lGODlhAQABAPAA${
+		triplet(0, r, g) + triplet(b, 255, 255)
+	}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
 export function EpisodeCard({
 	episode,
 	className,
@@ -46,6 +59,8 @@ export function EpisodeCard({
 					alt="episode"
 					fill={true}
 					priority={true}
+					placeholder="blur"
+					blurDataURL={rgbDataURL(255, 0, 0)}
 					sizes="576px"
 					className="absolute object-cover object-top z-0 rounded-3xl brightness-40 filter"
 				/>
