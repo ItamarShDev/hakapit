60f859c28721dff1890d491df27a791883ec502b
Itamar Sharify <itamarsharify@gmail.com>
2024-08-27 23:41:47 +0300

    feat: update layout components to use PodcastName type for params, ensuring better type safety across layouts

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -1,12 +1,13 @@
 import { PlayerProvider } from "~/components/player/provider";
 import { MainLayout } from "~/layouts/main";
+import type { PodcastName } from "~/server/rss/feed";
 
 export default function RootLayout({
 	children,
 	params: { podcast },
 }: {
 	children: React.ReactNode;
-	params: { podcast: string };
+	params: { podcast: PodcastName };
 }) {
 	return (
 		<MainLayout params={{ podcast }}>
--- app/[podcast]/layout.tsx
+++ app/[podcast]/layout.tsx
@@ -1,8 +1,9 @@
 import Script from "next/script";
 import { PlayerProvider } from "~/components/player/provider";
 import { MainLayout } from "~/layouts/main";
+import type { PodcastName } from "~/server/rss/feed";
 
-export default function Layout({ children, params }: { children: React.ReactNode; params: { podcast: string } }) {
+export default function Layout({ children, params }: { children: React.ReactNode; params: { podcast: PodcastName } }) {
 	const id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 	return (
 		<MainLayout params={params}>
